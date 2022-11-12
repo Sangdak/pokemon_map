409b66e4e093eb36de65e4a21a583ca314ff46d1
Sangdak <82779027+Sangdak@users.noreply.github.com>
2022-11-12 22:44:56 +1000

    course_orm__lsn_4 - stp_19 - added to models associate verbose_names.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,18 +2,19 @@ from django.db import models  # noqa F401
 
 
 class Pokemon(models.Model):
-    title_ru = models.CharField(max_length=200)
-    title_en = models.CharField(max_length=200, blank=True)
-    title_jp = models.CharField(max_length=200, blank=True)
-    description = models.TextField(blank=True)
+    title_ru = models.CharField('Название', max_length=200)
+    title_en = models.CharField('Название англ.', max_length=200, blank=True)
+    title_jp = models.CharField('Название яп.', max_length=200, blank=True)
+    description = models.TextField('Описание', blank=True)
     previous_evolution = models.ForeignKey(
         'pokemon_entities.pokemon',
+        verbose_name='Из кого эволюционирует',
         related_name='next_evolution',
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
     )
-    img_url = models.ImageField(null=True)
+    img_url = models.ImageField('Изображение', null=True)
 
     def __str__(self):
         return self.title_ru
@@ -21,10 +22,10 @@ class Pokemon(models.Model):
 
 class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
-    lat = models.FloatField(blank=True)
-    lon = models.FloatField(blank=True)
-    appeared_at = models.DateTimeField(blank=True)
-    disappeared_at = models.DateTimeField(blank=True)
+    lat = models.FloatField('Координаты, шир.', blank=True)
+    lon = models.FloatField('Координаты, долг.', blank=True)
+    appeared_at = models.DateTimeField('Время появления', blank=True)
+    disappeared_at = models.DateTimeField('Время исчезновения', blank=True)
     level = models.IntegerField(verbose_name='Уровень', default=0, blank=True)
     health = models.IntegerField(verbose_name='Здоровье', default=0, blank=True)
     attack = models.IntegerField(verbose_name='Атака', default=0, blank=True)
